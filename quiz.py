6c8c3f3bb078fcfbc91304546282c1ea48f0eb77
Itsparrotdude <noah.volante@gmail.com>
2023-08-16 10:10:26 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -13,7 +13,37 @@ quiz = {
     "2x + 8 = 18": ["5", "6", "7", "4"],
 }
 
-for question, answer in quiz.items():
-    correct_answer = answer[0]
+subjects = "Math, Not currently supported: Digital Technology, Biology, Chemistry, Physics, Statistics, Physical Education, Health and Economics!"
 
 
+score = 0
+
+print("Welcome to Quiz Central!")
+name = input("Please enter you name: ")
+yearlvl = input(f"Thanks {name}, please enter your year level: ")
+
+while yearlvl.isdigit():
+    if yearlvl == "11":
+        print("Welcome to the Y11 Quiz Central;")
+        print(f"Here is a list of all the Year 11 subjects: {subjects}" )
+        studysubject = input("What subject would you like to study?: ")
+        if studysubject == "Math":
+            for question, answer in quiz.items():
+                correct_answer = answer[0]
+                random.shuffle(answer)
+                print(question)
+                for answernum, option in enumerate(answer, start=1):
+                    print(f"{answernum}) {option}")
+                answer_label = int(input("Your answer: "))
+                user_input = (answer[answer_label - 1])
+                if user_input == correct_answer:
+                    print("Correct")
+                    score += 1
+                else:
+                    print(f"Incorrect! The answer was {correct_answer} not {user_input}!")
+
+        print(f"Your final score is: {score}/{len(quiz)}")  
+    else:
+        print("Sorry but Quiz Central is not currently supported in your region, please make a request to our offices in Mumbai for support in your year level!")
+else:
+    print("Please enter an integer in between 0-13!")
