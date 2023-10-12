1d18c9a6748f0b9b5008f11507143383d092a8cd
Itsparrotdude <noah.volante@gmail.com>
2023-10-13 08:33:57 +1300

    Fixed all the convention errors

--- quiz.py
+++ quiz.py
@@ -1,13 +1,11 @@
+"""This module is for randomly swapping answers."""
 import random
 
-# JUMBODICT = {
-#     "Math": ["Math Quiz #1 (Easy)": {"x + 2 = 5": ["3", "2", "4", "5"]} ]
-# }
 
 mathquiz1y11 = {
-    "x + 2 = 5": [ "3", "2", "4", "5"],
-    "5x = 10": [ "2", "15", "50", "0.5" ],
-    "3x + 3 = 21": [ "6", "8", "15", "4"],
+    "x + 2 = 5": ["3", "2", "4", "5"],
+    "5x = 10": ["2", "15", "50", "0.5"],
+    "3x + 3 = 21": ["6", "8", "15", "4"],
     "2x - 7 = 11": ["9", "7", "4", "6"],
     "4 + 3x = 19": ["5", "4", "7", "6"],
     "6x - 9 = 33": ["7", "8", "6", "9"],
@@ -30,10 +28,12 @@ mathquiztitles = [
 subjects = [
     "Math",
     "Not currently supported:",
-    "Digital Technology", 
-    "Biology, Chemistry", 
-    "Physics, Statistics", 
-    "Physical Education", 
+    "Digital Technology",
+    "Biology",
+    "Chemistry",
+    "Physics",
+    "Statistics",
+    "Physical Education",
     "Health",
     "Economics!"
 ]
@@ -49,48 +49,56 @@ while True:
     if yearlvl.isdigit():
         if yearlvl == "11":
             print("Welcome to the Y11 Quiz Central!")
-            print(f"Here is a list of all the Year 11 subjects:")
-            for avalaiblesubjectsy11, subjectnamey11 in enumerate(subjects, start = 1):
+            print("Here is a list of all the Year 11 subjects:")
+            for avalaiblesubjectsy11, subjectnamey11 in enumerate(subjects, start=1):
                 print(f"{avalaiblesubjectsy11}) {subjectnamey11}")
             while True:
                 studysubject = input("What subject would you like to study?: ")
-                if studysubject == "Math" or "1":
+                if studysubject == "Math" or studysubject == "1":
                     print("Welcome to the Quiz Centrals Math hub!")
                     print("Here is a list of all the avaiable Year 11 Math quizzes:")
-                    for avalaiblemathy11quiz, quiznamemathy11 in enumerate(mathquiztitles, start = 1):
+                    for avalaiblemathy11quiz, quiznamemathy11 in enumerate(mathquiztitles, start=1):
                         print(f"{avalaiblemathy11quiz}) {quiznamemathy11}")
                     while True:
                         mathquizuserchoice = input("Please select the quiz you would like to take? ")
                         if mathquizuserchoice == "1":
+                            print("You have chosen chosen Math Quiz #1, let's begin!")
                             for question, answer in mathquiz1y11.items():
                                 correct_answer = answer[0]
                                 random.shuffle(answer)
-                                
+
                                 print(f"Question: {question}")
                                 for answernum, option in enumerate(answer, start=1):
                                     print(f"{answernum}) {option}")
-                                answer_label = int(input("Your answer: "))                                   
-                                user_input = (answer[answer_label - 1])
-                        
+
+                                while True:
+                                    try:
+                                        answer_label = int(input("Your answer: "))
+                                        user_input = (answer[answer_label - 1])
+                                        break
+                                    except ValueError:
+                                        print("Please enter a valid answer!")
+                                    except IndexError:
+                                        print("Please enter a valid answer!")
+
                                 if user_input == correct_answer:
                                     print("Correct")
                                     score += 1
                                 else:
                                     print(f"Incorrect! The answer was {correct_answer} not {user_input}!")
 
-                            print(f"Your final score is: {score}/{len(mathquiz1y11)}") 
+                            print(f"Nice one {name}! Your final score is: {score}/{len(mathquiz1y11)}")
 
-                        else: 
+                            score = 0
+
+                        else:
                             print("Please select a proper Math Quiz!")
-                else: 
-                    print("Please choose an available subject!")  
+                else:
+                    print("Please choose an available subject!")
                     studysubject = input("What subject would you like to study?: ")
-            
-                    
-            
+
         else:
             print(f"Sorry but Quiz Central is not currently supported for Y{yearlvl}, please make a request to our offices in Mumbai for support in your year level!")
-    
+
     else:
         yearlvl = print("Please enter an integer in between 0-13!: ")
-        
